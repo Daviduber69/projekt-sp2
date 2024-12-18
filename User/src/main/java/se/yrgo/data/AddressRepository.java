f6cd531106de5d8a3bb8ac9db1457396b66aba0b
Daviduber69 <davhen0108@skola.goteborg.se>
2024-12-18 16:31:53 +0100

    restructuring User to have it work on our react frontend with all API methods working

--- User/src/main/java/se/yrgo/data/AddressRepository.java
+++ User/src/main/java/se/yrgo/data/AddressRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.yrgo.domain.AddressEntity;
 
-@Repository
+
 public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
 }
